cf6b8b536aec4251872462ab957bcc94c67f1481
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2025-07-09 18:04:23 -0500

    My first hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+#function to run the app
+def main():
+    st.title("Hello World App")
+    #st.header("Welcome to my first Streamlit app!")
+
+# validate the app
+if __name__ == "__main__":
+    main() # run the app
+    
