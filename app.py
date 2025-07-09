32c4768fdf671b54f2f6b6a5cb5d32c36c8b8c5c
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2025-07-09 18:15:10 -0500

    Typing of text

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 #function to run the app
 def main():
-    st.title("Hello World App")
-    #st.header("Welcome to my first Streamlit app!")
+    st.title("Hello World App") #titule of the app https://docs.streamlit.io/develop/api-reference/text/st.title
+    st.header("Titulo de encabezado") # titulo encabezado https://docs.streamlit.io/develop/api-reference/text/st.header
+    st.subheader("Subtitulo de encabezado") #subtitulo https://docs.streamlit.io/develop/api-reference/text/st.subheader
+    st.text("Texto corriente") #text https://docs.streamlit.io/develop/api-reference/text/st.text
+    st.markdown("# Texto con markdown") #markdown https://docs.streamlit.io/develop/api-reference/text/st.markdown
+    st.markdown("## OtroTexto con markdown ") #markdown https://docs.streamlit.io/develop/api-reference/text/st.markdown
+    st.write("Texto de modo escritura") #write https://docs.streamlit.io/develop/api-reference/text/st.write
+    
 
 # validate the app
 if __name__ == "__main__":
