8f740962469dbbaafac18f7d8a1f8743bbc68eb3
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2025-07-10 13:44:36 -0500

    Adding multimedia- images

--- app.py
+++ app.py
@@ -1,44 +1,18 @@
 import streamlit as st 
-import pandas as pd
+from PIL import Image #importar la libreria PIL para manejar imagenes pill es una libreria de python para manejar imagenes y graficos https://pillow.readthedocs.io/en/stable/
+
 
 
 #function to run the app
 def main(): 
     # 1. ====Agregando Selectbox ===
-    st.title("Selección") # titulo de la app
-    opcion = st.selectbox(
-        'Selecciona una opción',
-        ['Coco', 'Piña', 'Avocado']
-    )
-    #mostrar la opción seleccionada
-    st.write(f'Has seleccionado:{opcion}')
+    st.title("Manejo de multimedia") # titulo de la app
+    img = Image.open("multimedia/imagen.png") # abrir la imagen con PIL
+    st.image(img, caption="Imagen de ejemplo", use_container_width = True) # mostrar la imagen en la app con un caption y ajustando el ancho de la columna https://docs.streamlit.io/library/api-reference/layout/st.image
     
-    # 2.=====MULTIPLE SELECTBOX=====
-    st.title("Selección Múltiple") # titulo de la app
-    opciones2 = st.multiselect(
-        'Selecciona una o más opciones',
-        ['Coco', 'Piña', 'Avocado']
-    )
-    #mostrar las opciones seleccionadas
-    st.write(f'Has seleccionado: {opciones2}')
-    
-    # 3. =====SLIDER=====
-    st.title("Slider") # titulo de la app
-    valor = st.slider(
-        'Seleccione su edad',
-        0, 100, 25 # min, max, default        
-    )
-    #mostrar el valor seleccionado
-    st.write(f'Has seleccionado: {valor}')
-    
-    # ===Select Slider ===
-    st.title("Select Slider") # titulo de la app
-    opciones3 = st.select_slider(
-        'Selecciona su nivel de satisfacción',
-        options=['Muy bajo', 'Bajo', 'Medio', 'Alto', 'Muy alto'],
-        value='Medio' # valor por defecto       
-    )
-    st.write(f'Has seleccionado: {opciones3}')
+    #=====Mostrar imagen radom de internet====
+    st.subheader("Imagen aleatoria de internet") # subtitulo de la seccion
+    st.image("https://picsum.photos/150/150", caption="Imagen aleatoria de internet", use_container_width = True) # mostrar una imagen aleatoria de internet con un caption y ajustando el ancho de la columna https://docs.streamlit.io/library/api-reference/layout/st.image
     
     
 # validate the app
